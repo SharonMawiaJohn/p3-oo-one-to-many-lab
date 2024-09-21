cd058b2aa351190903fd3ff0da9b2d113861bfe2
SharonMawiaJohn <sharonmawia76@gmail.com>
2024-09-21 20:35:18 +0300

    Add the changes

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,28 @@
 class Pet:
-    pass
+    all = []
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    
+    def __init__(self, name, pet_type, owner=None):
+       if pet_type not in Pet.PET_TYPES:
+            raise Exception(f"Invalid pet type: {pet_type}. Not on the list of pets ")
+       
+       self.name = name
+       self.pet_type = pet_type
+       self.owner = owner
+       Pet.all.append(self)
+        
 
 class Owner:
-    pass
+    def __init__(self,name):
+        self.name = name
+
+    def pets(self):
+        return[pet for pet in Pet.all if pet.owner == self]
+    
+    def add_pet(self, pet):
+         if not isinstance(pet, Pet):
+            raise TypeError("Pet must be an instance of Pet class")
+         pet.owner = self
+
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key=lambda pet: pet.name)
